928ba5a16e414c8a8ac785c66e770b3edabe6307
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-21 02:28:01 +0000

    Actividad

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,19 @@
 package main
 
-func Burbuja(s []int64)  {
 
+func burbuja(Lista []int){
+ var auxiliar int
+ for i := 0; i < len(Lista); i++ {
+  for j := 0; j < len(Lista); j++ {
+   if Lista[i] < Lista[j] {
+    auxiliar = Lista[i]
+    Lista[i] = Lista[j]
+    Lista[j] = auxiliar
+    }
+  }
+}
 }
 
-func main()  {
-	
+func main() {
+  
 }
