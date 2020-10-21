fc65e5f910032ba4c7dc05ca49e72bb4f40bf291
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-21 02:29:34 +0000

    int64

--- burbuja.go
+++ burbuja.go
@@ -1,8 +1,8 @@
 package main
 
 
-func burbuja(Lista []int){
- var auxiliar int
+func burbuja(Lista []int64){
+ var auxiliar int64
  for i := 0; i < len(Lista); i++ {
   for j := 0; j < len(Lista); j++ {
    if Lista[i] < Lista[j] {
